0ff319ec322938a6aed371e9f41ca43323a8ef1a
harish-b-4 <harishb.contact@gmail.com>
2025-12-07 13:03:03 +0530

    Modified Moviecollentions

--- src/Moviecollections.jsx
+++ src/Moviecollections.jsx
@@ -1,11 +1,12 @@
 import Ageofultron from "./assets/Age of ultron.png"
 import Civilwar from "./assets/Civil war.png"
-import Daredevil from "./assets/DareDevil.png"
+import Daredevil from "./assets/Daredevil.png"
 import Defenders from "./assets/Defenders.png"
 import Endgame from "./assets/Endgame.png"
 import Fantasticfour from "./assets/Fantstic four.png"
 import Infinitywar from "./assets/Infinity war.png"
 import Ironman from "./assets/Iron man.png"
+
 
 const Moviecollections = () => {
     return (
@@ -16,7 +17,7 @@ const Moviecollections = () => {
             <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:gap-36 px-5 mt-5 py-2">
                 <img src={Ageofultron} alt="Age Of Ultron" />
                 <img src={Civilwar} alt="Civil War" />
-                <img src={Endgame} alt="EndGame" />
+                <img src={Endgame} alt="Endgame" />
                 <img src={Fantasticfour} alt="Fantastic Four" />
                 <img src={Infinitywar} alt="Infinity War" />
                 <img src={Ironman} alt="Iron man" />
@@ -25,7 +26,7 @@ const Moviecollections = () => {
             <h1 className="font-semibold text-3xl text-white ml-5 mt-10">Marvel Tv Shows</h1>
 
             <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:gap-36 px-5 mt-5 py-2">
-                <img src={Daredevil} alt="DareDevil" />
+                <img src={Daredevil} alt="Daredevil" />
                 <img src={Defenders} alt="Defenders" />
             </div>
 
